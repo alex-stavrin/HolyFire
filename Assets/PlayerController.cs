90acec300b65968fef1b0675dceff2d36ff16a24
Alekossta <alekosstavrinoudakis@gmail.com>
2025-04-07 08:11:46 +0300

    initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    InputAction moveAction;
+    InputAction lookAction;
+
+    void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Player/Move");
+        lookAction = InputSystem.actions.FindAction("Player/Look");
+    }
+
+    void Update()
+    {
+        Vector2 moveValue = moveAction.ReadValue<Vector2>();
+        Debug.Log(moveValue);
+    }
+}
